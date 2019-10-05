cc08eacaf9a0b857d50115adbef62b190c681e68
natishlom <Natishlom@gmail.com>
2019-10-06 00:56:14 +0300

    יקיר

--- EasyToSit/LoginPage.cs
+++ EasyToSit/LoginPage.cs
@@ -49,7 +49,7 @@ namespace EasyToSit
                     messageBox("שם המשתמש שהזנת אינו קיים, נא הזן שם המשתמש שנית", "שגיאה");
             }
             else
-                messageBox("לא הוכנס שם משתמש","שגיאה");
+                messageBox("יקיר","שגיאה");
         }
 
         private void txtPass_KeyDown(object sender, KeyEventArgs e)
